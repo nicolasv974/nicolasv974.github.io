00bab0090b23bd86c72e599e023baa3fc61e05be
Vyplasil Nicolas <nicolas.vyplasil@hotmail.com>
2023-10-25 12:02:14 +0400

    icon link github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Autres configurations...
+
+    routes: [
+        {
+            path: '/api/contact', // L'URL de la page du formulaire
+            load: '/pages/contact.astro' // L'emplacement du fichier Astro correspondant
+        },
+        // Autres routes...
+    ]
+});
